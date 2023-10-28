cf831801cf3a4b52f31e0304b7414909f99f0928
lokesh082155 <lokeshhachiko@gmail.com>
2023-10-29 01:56:53 +0530

    Merge branch 'master' of https://github.com/lokesh082155/voting-application.git

--- src/main/java/com/example/demo/AwsEksVotingappApplication.java
+++ src/main/java/com/example/demo/AwsEksVotingappApplication.java
@@ -8,6 +8,7 @@ public class AwsEksVotingappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsEksVotingappApplication.class, args);
+		System.err.println("loki");
 	}
 
 }
